6c5dc064d1de311f4a2abb5889a1233bb5c2c83a
Jacob Lewis <jacob@jaso5.com>
2024-05-28 19:04:57 +0100

    updated things

--- backup.sh
+++ backup.sh
@@ -1,3 +1,3 @@
-cp -r ~/printer_data/config/ config/
-cp /boot/config.txt extra/boot/
-cp /etc/rc.local extra/etc/
+cp -r ~/printer_data/config/ ~/MyVoronConfig/config/
+cp /boot/config.txt ~/MyVoronConfig/extra/boot/
+cp /etc/rc.local ~/MyVoronConfig/extra/etc/
--- config/config/README.md
+++
@@ -1,4 +0,0 @@
-# VORON CONFIG
-
-## Changing nozzles
-Just uncomment the relevant nozzles in printer.cfg
